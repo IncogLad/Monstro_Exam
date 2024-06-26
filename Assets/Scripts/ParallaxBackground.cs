de7beee5529500da276b5a060d0c0b950e5bb112
IncogLad <vincenthe5th@yahoo.com>
2024-06-26 18:13:17 +0800

    parallax finished

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Camera cam;
+    private float startPos;
+    private float length;
+
+    [SerializeField] private float lengthOffset = 1f;
+    [SerializeField] private float scrollSpeed;
+    
     void Start()
     {
-        
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x * lengthOffset;
+        cam = Camera.main;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float temp = cam.transform.position.x * (1 - scrollSpeed);
+        float distance = cam.transform.position.x * scrollSpeed;
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length)
+        {
+            startPos += length;
+        }
+        else if (temp < startPos - length)
+        {
+            startPos -= length;
+        }
+
     }
 }
